47e75e81e1fdce1ede4f3e0c2f8fc9e214814bd5
xinfeng-tech <qianxinfeng@meituan.com>
2019-09-04 19:01:17 +0800

    update:文件组织

--- packages/app_base/lib/src/widgets/page.dart
+++ packages/app_base/lib/src/widgets/page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/// 页面组件抽象类
+/// 有状态页面组件抽象类
 abstract class StatefulPageWidget extends StatefulWidget{
   final String title;
   final bool isShowTitle;
@@ -14,6 +14,24 @@ abstract class StatefulPageState<T extends StatefulPageWidget> extends State<T>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: widget.isShowTitle?AppBar(title: Text(widget.title)):null,
+      body: buildBodyWidget(context),
+    );
+  }
+  @protected
+  Widget buildBodyWidget(BuildContext context);
+}
+
+/// 无状态页面组件抽象类
+abstract class StatelessPageWidget extends StatelessWidget{
+  final String title;
+  final bool isShowTitle;
+
+  StatelessPageWidget({Key key, this.title='Untitled',this.isShowTitle=true,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: this.isShowTitle?AppBar(title: Text(this.title)):null,
       body: buildBodyWidget(context),
     );
   }
--- packages/app_base/lib/widgets.dart
+++ packages/app_base/lib/widgets.dart
@@ -1,6 +1,5 @@
 library app_base.widgets;
 
-export 'src/widgets/stateful_page.dart';
-export 'src/widgets/stateless_page.dart';
+export 'src/widgets/page.dart';
 export 'src/widgets/loading.dart';
 export 'src/widgets/toast.dart';
