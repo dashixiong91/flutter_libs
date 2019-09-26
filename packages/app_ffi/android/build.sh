d7f9a71b7088c7dbda69c936652d8643c2dab9f2
xinfeng-tech <616782041@qq.com>
2019-09-26 15:28:32 +0800

    add:split flutter-platform and ffi gradle plugin

--- packages/app_ffi/android/build.sh
+++ packages/app_ffi/android/build.sh
@@ -4,8 +4,6 @@ set -e
 
 THIS_DIR="$(cd "$(if [[ "${0:0:1}" == "/" ]]; then echo "$(dirname $0)";else echo "$PWD/$(dirname $0)";fi)"; pwd)"
 
-FLUTTER_ENGINE_DIR="$FLUTTER_ROOT/bin/cache/artifacts/engine"
-FLUTTER_ENGINE_JAR="$FLUTTER_ENGINE_DIR/android-arm/flutter.jar"
 PROJECT_DIR="$THIS_DIR"
 PROJECT_LIBS_DIR="${PROJECT_DIR}/libs"
 BUILD_DIR="$PROJECT_DIR/../build/android"
@@ -13,17 +11,33 @@ BUILD_DIR="$PROJECT_DIR/../build/android"
 function clean(){
   rm -rf ${BUILD_DIR}*
   rm -rf ${PROJECT_LIBS_DIR}
-  mkdir -p ${PROJECT_LIBS_DIR}
-  cp -f ${FLUTTER_ENGINE_JAR} ${PROJECT_LIBS_DIR}
 }
-function gradle_build(){
+function build_aar(){
   pushd ${PROJECT_DIR}
   ./gradlew -PbuildDir=${BUILD_DIR} build
   popd
 }
+function init_dependencies(){
+  mkdir -p ${PROJECT_LIBS_DIR}
+  local flutter_jar="$FLUTTER_ROOT/bin/cache/artifacts/engine/android-arm/flutter.jar"
+  cp -f ${flutter_jar} ${PROJECT_LIBS_DIR}
+  echo "INIT JSC:DEPENDENCY DONE (ANDROID)   ========="
+}
+
 function main(){
+  local cmd args
+  cmd="$1"
+  args="${@#$cmd}"
   clean
-  gradle_build
-  echo "BUILD SUCCESSFUL (ANDROID)   ========="
+  case ${cmd} in
+      "init_dependencies")
+        init_dependencies $args
+      ;;
+      "build"|*)
+        init_dependencies $args
+        build_aar
+        echo "BUILD SUCCESSFUL (ANDROID)   ========="
+      ;;
+  esac
 }
 main "$@"
