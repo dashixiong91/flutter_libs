75acfac25c378c1b36e584a823d39359c0129e8a
xinfeng-tech <616782041@qq.com>
2019-09-28 21:51:39 +0800

    format

--- packages/app_ffi/ios/cpp/src/app-ffi.cc
+++ packages/app_ffi/ios/cpp/src/app-ffi.cc
@@ -2,27 +2,22 @@
 // Created by xinfeng on 2019-09-16.
 //
 #include "app-ffi.h"
-#include <iostream>
 #include <ctime>
+#include <iostream>
 using namespace std;
 
-namespace jsc
-{
-int add(int a, int b)
-{
+namespace jsc {
+int add(int a, int b) {
   return a + b;
 }
-struct Books
-{
+struct Books {
   char title[50];
   char author[50];
   char subject[100];
   int book_id;
 } book;
-} // namespace jsc
+}  // namespace jsc
 
-int main()
-{
-  
+int main() {
   return 0;
 }
