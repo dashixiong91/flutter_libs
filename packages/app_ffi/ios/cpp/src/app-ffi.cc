fd686a551ee14c6b55450b69eea9e373fe77b7d0
xinfeng-tech <616782041@qq.com>
2019-09-29 08:46:47 +0800

    update:build config

--- packages/app_ffi/ios/cpp/src/app-ffi.cc
+++ packages/app_ffi/ios/cpp/src/app-ffi.cc
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
+using namespace jsc;
 
 namespace jsc {
 int add(int a, int b) {
@@ -19,5 +20,31 @@ struct Books {
 }  // namespace jsc
 
 int main() {
+   Books Book1;        // 定义结构体类型 Books 的变量 Book1
+   Books Book2;        // 定义结构体类型 Books 的变量 Book2
+ 
+   // Book1 详述
+   strcpy( Book1.title, "C++ 教程");
+   strcpy( Book1.author, "Runoob"); 
+   strcpy( Book1.subject, "编程语言");
+   Book1.book_id = 12345;
+ 
+   // Book2 详述
+   strcpy( Book2.title, "CSS 教程");
+   strcpy( Book2.author, "Runoob");
+   strcpy( Book2.subject, "前端技术");
+   Book2.book_id = 12346;
+ 
+   // 输出 Book1 信息
+   cout << "第一本书标题 : " << Book1.title <<endl;
+   cout << "第一本书作者 : " << Book1.author <<endl;
+   cout << "第一本书类目 : " << Book1.subject <<endl;
+   cout << "第一本书 ID : " << Book1.book_id <<endl;
+ 
+   // 输出 Book2 信息
+   cout << "第二本书标题 : " << Book2.title <<endl;
+   cout << "第二本书作者 : " << Book2.author <<endl;
+   cout << "第二本书类目 : " << Book2.subject <<endl;
+   cout << "第二本书 ID : " << Book2.book_id <<endl;
   return 0;
 }
