18f4744e294acf4fa9347896ab8e84d366c2d334
xinfeng-tech <616782041@qq.com>
2019-10-02 00:06:12 +0800

    add:test cpp script

--- packages/app_ffi/ios/cpp/include/app-ffi.h
+++ packages/app_ffi/ios/cpp/include/app-ffi.h
@@ -5,16 +5,28 @@
 #ifndef APP_FFI_H
 #define APP_FFI_H
 
+// FFI_EXTERN_C MACROS
 #ifdef __cplusplus
-extern "C" {
+#define FFI_EXTERN_C extern "C"
+#else
+#define FFI_EXTERN_C
+#endif
+// FFI_EXPORT MACROS
+#if defined(__CYGWIN__)
+#error Tool chain and platform not supported.
+#elif defined(_WIN32)
+#error Tool chain and platform not supported.
+#elif  __GNUC__ >= 4
+#define FFI_EXPORT FFI_EXTERN_C __attribute__((visibility("default"))) __attribute((used))
+#else
+#define FFI_EXPORT FFI_EXTERN_C
 #endif
 
-int add(int, int);
+// api start line
+FFI_EXPORT int add(int,int);
 
-class Box;
 
 
-#ifdef __cplusplus
-}
-#endif
-#endif  // APP_FFI_H
+// api end line
+
+#endif //APP_FFI_H
