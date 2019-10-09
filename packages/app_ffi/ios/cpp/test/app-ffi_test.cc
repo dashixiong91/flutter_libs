4a380248882c369df29e12734a9376145b1f114e
xinfeng-tech <616782041@qq.com>
2019-10-09 16:59:55 +0800

    update:cpp test

--- packages/app_ffi/ios/cpp/test/app-ffi_test.cc
+++ packages/app_ffi/ios/cpp/test/app-ffi_test.cc
@@ -1,3 +1,6 @@
+//
+// Created by xinfeng on 2019-09-16.
+//
 #include "app-ffi.h"
 #include "iostream"
 using namespace std;
@@ -5,7 +8,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
   int result = add(1,2);
-  cout<<"add(1,2)"<<endl;
-  cout<<result<<endl;
+  cout << "add(1,2)=>" << result<< endl;
+  if(result!=3){
+    return 1;
+  }
   return 0;
 }
+
+
