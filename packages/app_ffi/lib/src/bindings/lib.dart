55bfd83dc6ddcb177b469c003b6e6176b7c5f607
xinfeng-tech <616782041@qq.com>
2019-11-05 19:02:34 +0800

    style: pull out const var to consts.dart

--- packages/app_ffi/lib/src/bindings/lib.dart
+++ packages/app_ffi/lib/src/bindings/lib.dart
@@ -1,6 +1,5 @@
 import 'dart:ffi' as ffi;
+import 'consts.dart';
 import '../utils/dylib.dart';
-
-const String LIBRARY_NAME='app_ffi';
 
 ffi.DynamicLibrary lib = DynamicLibraryHelp.load(LIBRARY_NAME);
--- packages/app_ffi/lib/src/utils/dylib.dart
+++ packages/app_ffi/lib/src/utils/dylib.dart
@@ -1,14 +1,14 @@
 import 'dart:ffi' as ffi;
 import 'dart:io' show Platform;
-
-import 'package:flutter/cupertino.dart';
+import '../bindings/consts.dart';
 
 typedef LibLoader = ffi.DynamicLibrary Function(String libName);
 
 class DynamicLibraryHelp {
   DynamicLibraryHelp._();
   static LibLoader libLoader;
-  static final Map<String, ffi.DynamicLibrary> _libs = <String, ffi.DynamicLibrary>{};
+  static final Map<String, ffi.DynamicLibrary> _libs =
+      <String, ffi.DynamicLibrary>{};
 
   static ffi.DynamicLibrary load(String libName) {
     String fullLibName = 'lib$libName.so';
@@ -16,16 +16,24 @@ class DynamicLibraryHelp {
       fullLibName = '$libName.framework/$libName';
     }
     if (!_libs.containsKey(fullLibName)) {
-      assert((){
-        debugPrint('ffi:$fullLibName is loading');
+      assert(() {
+        print('ffi:$fullLibName is loading');
         return true;
       }());
+      ffi.DynamicLibrary lib;
       if (libLoader != null) {
-        _libs[fullLibName] = libLoader(fullLibName);
-      } else {
-        _libs[fullLibName] = ffi.DynamicLibrary.open(fullLibName);
+        lib = libLoader(fullLibName);
       }
+      if (lib == null) {
+        // use DynamicLibrary if libLoader not specificed.
+        lib = ffi.DynamicLibrary.open(fullLibName);
+      }
+      _libs[fullLibName] = lib;
     }
     return _libs[fullLibName];
+  }
+
+  static LibLoader get staticLibLoader {
+    return (String libName) => libName.contains(LIBRARY_NAME) && Platform.isIOS ? ffi.DynamicLibrary.executable() : null;
   }
 }
